60f432239bfedfd4b4341eff3b24240000a4c593
Tejaswi <dollydhyani6@gmail.com>
2023-05-16 10:19:46 -0700

    My first comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Card from "./Card";
+import Sdata from "./Sdata";
 
-function App() {
+
+// export default function App() {
+//   return (
+//    <>
+//    <div className="AppBody">
+//     <div className="headingbar  bg-light py-3 mb-5 ">
+//       <h2 className="text-center"> Top Netflix Series 2022</h2>
+//     </div>
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[0].imgsrc}
+// sname={Sdata[0].sname} 
+// slink={Sdata[0].slink}
+//           /> 
+//         </div>
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[1].imgsrc}
+// sname={Sdata[1].sname} 
+// slink={Sdata[1].slink}
+//           /> 
+//         </div>
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[2].imgsrc}
+// sname={Sdata[2].sname} 
+// slink={Sdata[2].slink}
+//           /> 
+//         </div>
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[3].imgsrc}
+// sname={Sdata[3].sname} 
+// slink={Sdata[3].slink}
+//           /> 
+//         </div>
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[4].imgsrc}
+// sname={Sdata[4].sname} 
+// slink={Sdata[4].slink}
+//           /> 
+//         </div>
+//         <div className="col-md-4 mb-4">
+//         <Card imgsrc={Sdata[5].imgsrc}
+// sname={Sdata[5].sname} 
+// slink={Sdata[5].slink}
+//           /> 
+//         </div>
+//       </div>
+    
+   
+
+//     </div>
+   
+    
+   
+//    </div>
+
+   
+//    </>
+//   );
+// }
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div className="AppBody">
+    <div className="headingbar  bg-light py-3 mb-5 ">
+      <h2 className="text-center"> Top Netflix Series 2022</h2>
     </div>
+    <div className="container">
+      <div className="row">
+        
+        
+          {
+            Sdata.map(function ncard(val){
+              return(
+                <div className="col-md-4 mb-4">
+                  <Card imgsrc={val.imgsrc}
+                sname={val.sname} 
+                slink={val.slink}
+                          /> 
+                </div>
+                
+              );
+             
+            })
+          }
+        </div>
+      
+    </div>
+  </div>
+</>
   );
 }
-
-export default App;
---
+++ src/Sdata.js
@@ -0,0 +1,36 @@
+ const Sdata=[
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        sname: "DARK" ,
+        slink: "https://www.netflix.com/in/title/80100172"
+
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BMzM2NTAzMzYtNWVlNi00NzFmLWE3MjgtMWI5NWI2MjAzZjdmXkEyXkFqcGdeQXVyODE2MzY0ODM@._V1_FMjpg_UX1000_.jpg",
+        sname:"Extra Curricular" ,
+        slink:"https://www.netflix.com/in/title/80990668" 
+    },
+    {
+        imgsrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Stranger_Things_logo.png/640px-Stranger_Things_logo.png",
+        sname:"Stranger Things" ,
+        slink:"https://www.netflix.com/in/title/80057281"
+    },
+    {
+        imgsrc:"https://m.economictimes.com/thumb/msid-94425391,width-1060,height-1268,resizemode-4,imgsize-104160/monster-the-jeffrey-dahmer-story-who-is-glenda-cleveland.jpg",
+        sname:"DAHMER - Monster" ,
+        slink:"https://www.netflix.com/in/title/81287562"
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BMjA1N2Y0MGItNWJlNy00Yzg3LWE3MmYtODRmYmM5NjZlNDc4XkEyXkFqcGdeQXVyMTMxNjUyMDkx._V1_.jpg",
+        sname:"Dynasty" ,
+        slink:"https://www.netflix.com/in/title/80179394" 
+    },
+
+    {
+        imgsrc:"https://resizing.flixster.com/zhPYdxVDydROcEPqja0dRu2cW7k=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvNDNjZjMyMDQtNzZiYi00MmM2LTlhZDktZjUyMDdiNTE5NjI5LmpwZw==",
+        sname:"Cobra Kai" ,
+        slink:"https://www.netflix.com/in/title/81002370" 
+    }
+]
+
+export default Sdata;
